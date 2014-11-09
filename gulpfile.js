a7c9e3a7417b8f63df60b57ce1a66651e8b3515b
jamondouglas <terrildouglas@gmail.com>
2014-11-08 17:07:35 -0800

    Clean up of package.json file for karmajs

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,26 @@
+//Include gulp
 var gulp = require('gulp');
 
 //Include Our Plugins
 var jsdoc = require('gulp-jsdoc');
 var jasmine = require('gulp-jasmine');
+var nodemon = require('gulp-nodemon');
+var jshint = require('gulp-jshint');
+
+
 
 var testFiles = [
   'server/**/*.js',
-  'test/**/*.js'
+  'test/**/**.js'
 ];
 
-gulp.task('default', function() {
+gulp.task('lint', function(){
+  gulp.src(testFiles)
+  .pipe(jshint('.jshintrc'))
+  .pipe(jshint.reporter('default', {verbose: true}))
+  .pipe(jshint.reporter('fail'))
+});
+
 gulp.task('test', function(){
   return gulp.src(testFiles)
       .pipe(jasmine());
@@ -19,4 +30,14 @@ gulp.task('docs', function() {
   gulp.src(['server/*.js', 'server/**/*.js'])
   .pipe(jsdoc('./docs'));
 });
+
+gulp.task('develop', function(){
+  nodemon({script:'./server/app.js', ext: 'js',})
+    .on('change', ['lint'])
+    .on('restart', function(){
+      console.log('restarted server!');
+    })
+})
+
+gulp.task('default', ['lint','develop']);
 
