ea83a4845d527ef70ed6de889fcd9b9134326179
mikephoran <mikephoran@gmail.com>
2014-11-18 19:12:52 -0800

    Rec Algo almost complete

--- server/db/recommendation.js
+++ server/db/recommendation.js
@@ -5,52 +5,94 @@ var Rating = require('./db').Rating;
 var TypesVendors = require('./db').TypesVendors;
 var TypesUsers = require('./db').TypesUsers;
 
+var rec = null;
 
-var UsersWithSameReviews = function(UserId) {
-  
-  Rating.findAll({where: {UserId:UserId}})
+var UsersWithSameReviews = function(requestedUser) {
+
+  var closestUser = null;
+
+
+  Rating.findAll({
+    where: {UserId:requestedUser},
+    order: '"VendorId"'
+  })
   .success(function(ratings) {
+    if (ratings.length < 3) {
+      closestUser = 'You must have reviewed at least 3 Vendors!'
+      return 
+    }
+
     var top3 = ratings.slice(0,4);
     var vendor1 = top3[0].VendorId;
     var vendor2 = top3[1].VendorId;
     var vendor3 = top3[2].VendorId;
+    var userinfo = {User: requestedUser, Vendor1: top3[0].rating, Vendor2: top3[1].rating, Vendor3: top3[2].rating};
+
     console.log('vendor1', vendor1, 'vendor2', vendor2, 'vendor3', vendor3);
 
-    User.findAll({
+    Rating.findAll({
       where: {VendorId: [vendor1, vendor2, vendor3]},
-      order: 'UserId DESC'
-    }).success(function(users){
-      console.log(users)
+      order: '"UserId", "VendorId"'
+    }).success(function(ratings){
+      var result = [];
+      for (var i=0; i<ratings.length; i++) {
+        var aUser = ratings[i].dataValues.UserId;
+        if (i+2 < ratings.length && aUser === ratings[i+2].dataValues.UserId && aUser !==requestedUser) {
+          result.push({User: aUser, Vendor1: ratings[i].dataValues.rating, Vendor2: ratings[i+1].dataValues.rating, Vendor3: ratings[i+2].dataValues.rating});
+          i+=3;
+        } 
+      }
+      console.log('User Being Queried', userinfo);
+      console.log('Neighbors', result);
+      if(!result) {
+        closestUser = 'You have not reviewed enough Vendors to make a recommendations!';
+        return;
+      } else if (result.length === 1 ) {
+        closestUser = result[0].User;
+        console.log(closestUser);
+      } else {
+        closestUser = findMostSimilar(userinfo, result);
+        console.log(closestUser);
+      }
     })
   })
-}
+};
+
+var calcSimilarity = function(a, b, max) {
+  var diff1 = max - a
+  var diff2 = max - b
+  var diff = Math.abs(diff2 - diff1)
+  var distance = diff / max
+  return 1 - distance
+};
+
+var avgSimilarity = function (user, neighbor, traits) {
+  var sum = 0;
+
+  for (var i=1; i<=traits; i++) {
+    sum += calcSimilarity(user['Vendor' + i], neighbor['Vendor' + i], 5);
+  }
+  return sum/traits;
+};
+
+var findMostSimilar = function(user, neighbors) {
+  var mostSimilar = 0;
+  var result = null;
+
+  for (var i=0; i<neighbors.length; i++) {
+    var similarity = avgSimilarity(user, neighbors[i], 3);
+    
+    if (similarity > mostSimilar) {
+      mostSimilar = similarity;
+      result = neighbors[i];
+    }
+  }
+
+  return result;
+};
+
 
 UsersWithSameReviews(4);
-
-// //select "UserId" from "Ratings" where "VendorId" = 81 AND "VendorId" = 91 AND "VendorId" = 93;
-
-//     // User.findAll({
-//     //   where: 
-
-//     // })
-//     // Rating.findAll({
-//     //   group: ['"UserId"', "id"], 
-//     //   where: sequelize.and({VendorId: [vendor1, vendor2, vendor3]})
-//     // })
-//     .success(function(results) {
-//       var similarUsers = [];;
-
-//       for (var i=0; i<results.length; i++) {
-//         similarUsers.push(results[i].UserId)
-//       }
-
-//       console.log(similarUsers);
-//     })
-
-
-//     //sequelize.query('SELECT * from "Users", "Ratings" where "Users".id = "Ratings"."UserId" and "Ratings.VendorId = Vendor1 and Ratings.VendorId = Vendor2 and Ratings.VendorId = Vendor3')
-// })
-// }
 
 
 
