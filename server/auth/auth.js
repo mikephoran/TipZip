23036af47abdf7818aa1e98a34311514d0140565
richardavendano <avendano.richard@gmail.com>
2014-11-19 14:58:24 -0800

    added change password functionality (WIP)

--- server/auth/auth.js
+++ server/auth/auth.js
@@ -5,4 +5,5 @@
 exports.login = require('./login').login;
 exports.isVendor = require('./login').isVendor;
 exports.logout = require('./login').logout;
-exports.register = require('./register');
+exports.register = require('./register').register;
+exports.changePassword = require('./register').changePassword;
--- server/auth/register.js
+++ server/auth/register.js
@@ -7,6 +7,7 @@ var helpers = require('../db/helpers');
 var User = require('../db/db').User;
 var bcrypt = require('bcrypt');
 var BPromise = require('bluebird');
+var checkPassword = require('./login').checkPassword;
 
 /**
 * Generates a new user and stores in DB
@@ -34,7 +35,57 @@ var generateUser = function(user) {
   });
 };
 
-module.exports = function(req, res) {
+/**
+* Generates a new user and stores in DB
+* @function
+* @memberof module:auth
+* @instance
+* @param {object} user User object profile to save to database.
+* @param {string} user.username Username of new user.
+* @param {string} user.email Email of new user.
+* @param {object} password Password Object for User.
+* @param {string} password.oldpw Old Password.
+* @param {string} password.newpw New Password.
+*/
+var changePassword = function(user, password) {
+  return new BPromise(function(resolve) {
+    helpers.getPersonal(user, function(user) {
+      helpers.checkPassword(user, password.oldpw)
+      .then(function(result) {
+        if (result) {
+          bcrypt.hash(password.newpw, 10, function(err, hash) {
+            user.updateAttributes({password: hash})
+            .success(resolve);
+          });
+        } else {
+          resolve(result);
+        }
+      });
+    });
+  })
+};
+
+exports.changePassword = function(req, res) {
+  var user = {
+    username: req.user,
+    email: req.user
+  };
+  var password = {
+    oldpw: req.body.oldPassword,
+    newpw: req.body.newPassword
+  };
+  changePassword(user, password)
+  .then(function(err) {
+    if (err) {
+      console.log('Password Change Failure:', err);
+      res.json({success: false, result: 'Invalid Password'});
+      return;
+    }
+    console.log('Password Change Success!');
+    res.json({success: true, result: 'Password Change Success'});
+  })
+}
+exports.register = function(req, res) {
   var newUser = {
     username: req.body.username,
     password: req.body.password,
@@ -52,3 +103,4 @@ module.exports = function(req, res) {
     });
   });
 };
+
